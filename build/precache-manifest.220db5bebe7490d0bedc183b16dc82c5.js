6ad0e42ceb89303170ad6489bbf63f14cd24e2e5
Jayjeet1013 <80195355+Jayjeet1013@users.noreply.github.com>
2022-03-13 21:30:50 +0530

    Add files via upload

---
+++ build/precache-manifest.220db5bebe7490d0bedc183b16dc82c5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5ba589791f9e643febeccca98a3230",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a125d71233c10089d699",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "8b179d8db0a3c193c5e3",
+    "url": "./static/css/main.0def4619.chunk.css"
+  },
+  {
+    "revision": "a125d71233c10089d699",
+    "url": "./static/js/2.04ba447e.chunk.js"
+  },
+  {
+    "revision": "3076bc301cd9ac8a6e5baf44c2b789b6",
+    "url": "./static/js/2.04ba447e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b179d8db0a3c193c5e3",
+    "url": "./static/js/main.5fefec8b.chunk.js"
+  },
+  {
+    "revision": "cd65410e499caeea6ae1",
+    "url": "./static/js/runtime-main.fe88992c.js"
+  },
+  {
+    "revision": "fc8d2280a067dd89ddd7af8dc1662eba",
+    "url": "./static/media/dice.fc8d2280.webp"
+  },
+  {
+    "revision": "53f49b581bd0dea8893033d43f28bd42",
+    "url": "./static/media/dice_logo.53f49b58.png"
+  },
+  {
+    "revision": "c0ab6380bf570f8c0ce0e632adb0a2c7",
+    "url": "./static/media/dice_rolling.c0ab6380.gif"
+  },
+  {
+    "revision": "cd488de327fe726ee152d0f43dce93ff",
+    "url": "./static/media/eth.cd488de3.png"
+  }
+]);
